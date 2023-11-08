a4daed45d7d15a9783b3c499c1f34c10a9e53ae2
Artezianec <artezianplanet@yandex.ru>
2023-11-08 23:36:59 +0200

    Change to Object Message

--- src/main/java/telran/java48/KarafkaConsumerApplication.java
+++ src/main/java/telran/java48/KarafkaConsumerApplication.java
@@ -5,28 +5,27 @@ import java.util.function.Consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import telran.java48.model.Message;
 
 @SpringBootApplication
 public class KarafkaConsumerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KarafkaConsumerApplication.class, args);
-	}
-	
-	@Bean
-	public Consumer<String> log() {
-		return message -> {
-			try {
-				Thread.sleep(3000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			String[] words = message.split(" ");
-			Long t2 = Long.valueOf(words[1]);
-			Long t = System.currentTimeMillis();
-			System.out.println("Received: " + words[0] + " Time: "+ (t-t2));
-		};
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KarafkaConsumerApplication.class, args);
+    }
+
+    @Bean
+    public Consumer<Message> log() {
+        return message -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            Long timeReceived = System.currentTimeMillis();
+            System.out.println("Received " +  message.getName() + " time the message was received " + (timeReceived-message.getTimeCreateMessage()));
+        };
+    }
 
 }
